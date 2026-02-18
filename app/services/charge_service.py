55e985e0b41f30ea37d32c36cfb24bf9fc97b0d9
Pepe-aduna <pepe.aduna@gmail.com>
2026-02-18 14:11:46 -0600

    mysql docker host

--- app/services/charge_service.py
+++ app/services/charge_service.py
@@ -16,6 +16,9 @@ class ChargeService:
     y en caso de cualquier error no controlado se rechace la petición y regrese
     el saldo a su estado anterior, como error controlado se incluye validación de error
     por fondos insuficientes, en tal caso se responde con el error correspondiente.
+    Al final se lanza un evento/mensaje/queue para registrar la data completa de la transaccion
+    en una base de datos no relacional para que lo relacionado con la consulta de transacciones
+    y los beneficios de este tipo de bases de datos sin los afectarnos por la eventual persistencia.
     """
 
     @staticmethod
@@ -37,6 +40,7 @@ class ChargeService:
                 print("rejected_by_charge")
                 return json.dumps({"error": response.get("error"), "tx_id": transaction.transaction_id})
 
+            ChargeService.event(data)
         except Exception as e:
             print(f"Exception in CHARGE: {e.__cause__}")
             if 'Insufficient' in str(e.__cause__):
@@ -54,3 +58,12 @@ class ChargeService:
         TransactionRepository.charge_reversal(transaction)
 
         return json.dumps({"message": "REVERSED", "tx_id": transaction.transaction_id})
+
+    @staticmethod
+    def event(data: dict):
+        """
+        Este metodo se utiliza para registrar en una base de datos no relacional
+        de forma asincrona los datos no escenciales para el ledger,
+        como descripciones, referencias, mcc, etc...
+        """
+        print("Registro asíncrono de transacciones")
