3ec8143eb7783769baa952291da1c715dd019719
Pepe-aduna <pepe.aduna@gmail.com>
2026-02-20 22:23:51 -0600

    secrets

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,6 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY . .
 CMD ["python", "run.py"]
+
+#Este dato vendría de algun servicio de manejo de contraseñas
+ENV DATABASE_URI=mysql+pymysql://clames:clames1!@host.docker.internal:3306/transaction_data
--- app/config.py
+++ app/config.py
@@ -1,12 +1,23 @@
 import os
+from flask import current_app
 
 class BaseConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+"""
+Con este método se obtiene la URL con las credenciales y el host para DB desde variables de entorno,
+si no se tienen limitantes, es mas practico cargar las variables consultando
+un servicio de Secrets de la nube GCP/AWS, al tener la carga de los secretos directo en el servicio
+y no en el despliegue nos permite mayor versatilidad para manejarlos
+y por ejemplo si tiene algún cambio se refrescan solo con reiniciar desde la misma nube 
+sin necesidad de redesplegar, que implica volver a ejecutar todo el flujo que se tenga de CICD.
+"""
+def get_value(key):
+    print(f"getting secret {key}: ")
+    return os.getenv(key)
+
 class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://clames:clames1!@host.docker.internal:3306/transaction_data"
-    )
+    SQLALCHEMY_DATABASE_URI = get_value('DATABASE_URI')
     DEBUG = True
 
 class ProductionConfig(BaseConfig):
